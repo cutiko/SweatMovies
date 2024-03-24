48b438286393c1dcea1f5c09da1341eacb447abf
cutiko <cutiko@gmail.com>
2024-03-24 16:58:13 -0300

    documentation

--- app/src/main/java/com/example/sweatmovies/models/MovieCategories.kt
+++ app/src/main/java/com/example/sweatmovies/models/MovieCategories.kt
@@ -3,6 +3,19 @@ package com.example.sweatmovies.models
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+/**
+ * This is a control table. We have to show different categories, so my first approach was to
+ * add an attribute to the [Movie], depending on the request then we can set that attribute.
+ * The problem is: what happen if a movie is in more than one category? So that changes
+ * the attribute to a collection of categories, and then everytime we have to update the [Movie]
+ * we have to carefully not override that category attribute, and then what happens if the movie
+ * is no longer in a category, everytime we request from a category we have to update every movie
+ * that is no longer in the category. Also, what happens when the user search for a movie?
+ * Considering such a complications: this is a control table for that. That way we can get
+ * the movies from a category from the DB for offline.
+ * Usually this should be documented else where but considering the circumstance I rather just
+ * write it here.
+ */
 @Entity
 data class MovieCategories(
     val nowPlaying: List<Int> = emptyList(),
